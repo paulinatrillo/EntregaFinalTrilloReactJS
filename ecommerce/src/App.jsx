ec8f658aebc299b14bc75b8fc057202061caef03
paulinatrillo <paulina.trillo@hotmail.com>
2023-04-08 01:21:50 -0300

    lista pre entrega 1

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -3,11 +3,12 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import NavBar from './components/NavBar/NavBar'
-
+import ItemListContainer from './components/ItemListContainer/ItemListContainer'
 function App() {
   return (
     <div className="App">
         <NavBar/>
+        <ItemListContainer greeting = {'¡Bienvenido/a!'} />
     </div>
   )
 }
