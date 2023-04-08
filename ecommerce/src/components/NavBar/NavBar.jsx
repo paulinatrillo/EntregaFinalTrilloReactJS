7b4323d6dd3b3cc9f4585ff6d5625da0251370d8
paulinatrillo <paulina.trillo@hotmail.com>
2023-04-08 00:35:05 -0300

    lista pre entrega 1

---
+++ ecommerce/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,14 @@
+const NavBar = () => {
+    return (
+       <nav>
+        <h3>E-commerce Antiguo Bazar</h3>
+         <div>
+        <button> Cómodas </button>
+        <button> Mesas de luz </button>
+        <button> Juegos de comedor </button>
+        </div>
+       </nav> 
+    )
+}
+
+export default NavBar
