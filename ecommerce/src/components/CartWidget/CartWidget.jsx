1b8fc0c72f3fe26dea6cd18bfbe8995f28f10ddf
paulinatrillo <paulina.trillo@hotmail.com>
2023-06-01 16:52:42 -0300

    entrega final correcciones

--- ecommerce/src/components/CartWidget/CartWidget.jsx
+++ ecommerce/src/components/CartWidget/CartWidget.jsx
@@ -10,7 +10,7 @@ const CartWidget = () => {
 const { totalQuantity } = useContext(CartContext)
 
   return (
-    <Link to='/cart' className='CartWidget' style={{ display: totalQuantity > 0 ? 'block' : 'none' }}>
+    <Link to='/carrito' className='CartWidget' style={{ display: totalQuantity > 0 ? 'block' : 'none' }}>
       <img className='CartImg' src={cart} alt="carrito.png" />
       {totalQuantity}
     </Link>
